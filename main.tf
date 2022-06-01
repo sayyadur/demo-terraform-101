2291a9a924c5f9e5659c7b763c31ab6c04c07b11
sayyadur <sayyadur@users.noreply.github.com>
2022-06-01 16:11:14 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,3 @@ terraform {
     }
   }
 }
-module "server" {
-   source = "./server"
-
-   num_webs     = var.num_webs
-   identity     = var.identity
-   ami          = var.ami
-   ingress_cidr = var.ingress_cidr
-   public_key   = var.public_key
-   private_key  = var.private_key
- }
